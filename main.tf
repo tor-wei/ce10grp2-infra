ff6400094df1f8ad7131144a089c12afc18de0b5
tor-wei <gohtorwei@gmail.com>
2025-07-26 20:14:08 +0800

    Fixed TFLint issues and added backend block

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "ce10grp2-infra.tfstate"
+    region = "ap-southeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
