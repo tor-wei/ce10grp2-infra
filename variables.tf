92c7bb0dba90d39d027bf5da351be6394fce0580
leoeric <leoeric13@gmail.com>
2025-07-27 15:16:37 +0800

    add image name replaced

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 locals {
   prefix             = "ce10grp2" # Set your desired prefix here
-  s3_container_name  = "ce10grp2-s3-service"
-  sqs_container_name = "ce10grp2-sqs-service"
+  s3_container_name  = "ce10grp2-s3-service-container"
+  sqs_container_name = "ce10grp2-sqs-service-container"
 }
