1c9f83bc40bc05be877f55b7012fa291d1552afa
tor-wei <gohtorwei@gmail.com>
2025-08-21 16:24:24 +0800

    Removed old infra code

--- ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "s3_service" {
-  name         = "${local.s3_service_name}-ecr"
-  force_delete = true
-}
-
-resource "aws_ecr_repository" "sqs_service" {
-  name         = "${local.sqs_service_name}-ecr"
-  force_delete = true
-}
