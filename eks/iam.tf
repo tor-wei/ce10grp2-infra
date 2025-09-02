8430de850f59c218a565e3b0cbc1eb76e443fb00
tor-wei <gohtorwei@gmail.com>
2025-09-03 05:28:38 +0800

    Added iam role for ExternalDNS

--- eks/iam.tf
+++ eks/iam.tf
@@ -14,38 +14,53 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_policy" "ecr_read_policy" {
-  name = "${var.name_prefix}-${var.env}-ecr-read-policy"
+data "aws_iam_policy_document" "ecr_read_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = [
-          "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.app_api_ecr_name}",
-          "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.app_frontend_ecr_name}"
-        ]
-      }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage"
     ]
-  })
+    resources = [
+      "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.app_api_ecr_name}",
+      "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.app_frontend_ecr_name}"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "external_dns_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["route53:ChangeResourceRecordSets"]
+    resources = ["arn:aws:route53:::hostedzone/*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResources"
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_role" "ecr_read_role" {
   name               = "${var.name_prefix}-${var.env}-ecr-read-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "ecr_read_policy" {
+  name   = "${var.name_prefix}-${var.env}-ecr-read-policy"
+  policy = data.aws_iam_policy_document.ecr_read_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecr_read_policy_attachment" {
@@ -61,4 +76,19 @@ resource "aws_iam_role" "ebs_csi_role" {
 resource "aws_iam_role_policy_attachment" "ebs_csi_policy_attachment" {
   role       = aws_iam_role.ebs_csi_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_iam_role" "external_dns_role" {
+  name               = "${var.name_prefix}-${var.env}-external-dns-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "external_dns_policy" {
+  name   = "${var.name_prefix}-${var.env}-external_dns-policy"
+  policy = data.aws_iam_policy_document.external_dns_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns_policy_attachment" {
+  role       = aws_iam_role.external_dns_role.name
+  policy_arn = aws_iam_policy.external_dns_policy.arn
 }
